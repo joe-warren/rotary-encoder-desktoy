74c78e35cf6ece323f495283ddb052165ce593f6
Joseph Warren <hungryjoe.warren@gmail.com>
2022-08-19 14:24:35 +0100

    add base

--- model.hs
+++ model.hs
@@ -5,20 +5,29 @@ import qualified Csg.STL
 import qualified Data.Text.IO as T
 
 rBevOuter = 10
-rBevInner = 6
-rDial = 60
+rBevInner = rBevOuter - wallT
+rDial = 60/2
+dialClearance = 1 
+wallT = 4
+dialInset = 5
 
-wPan = rDial + 20
+wPan = rDial*2 + 20
 dPan = wPan
 dTop = 10
 panAngle = pi/6
 eps = 0.001
-hBase = 20
+hBase = 15
+
+dTotal = dTop + dPan * cos panAngle 
 
 cylinder = Csg.unitCylinder 8
 sphere = Csg.unitSphere 8 4
 
 hTop = hBase + (dPan * sin panAngle)
+
+ledsCyl = Csg.scale (5/2, 5/2, 100) $ Csg.unitCylinder 12
+nLeds = 12
+shaftR= 6/2
 
 basicShape :: Double -> Csg.BspTree
 basicShape rBev  = Csg.unionConcat $ concat [
@@ -58,10 +67,54 @@ basicShape rBev  = Csg.unionConcat $ concat [
         in [s i | i <- [0.5, -0.5]]
   ]
 
-ovject :: Csg.BspTree
-object = basicShape rBevOuter `Csg.subtract` (Csg.translate (0, 0, -eps) $ basicShape rBevInner)
+ledHoles = Csg.unionConcat [
+              Csg.translate (0, ((fromInteger i - 0.5) * wPan / (fromInteger nLeds)) -wPan/2, 0) $ ledsCyl | i <- [1..nLeds]
+           ]
+
+
+-- TODO, confirm usb dims
+usbHole = Csg.translate (-dTop-rBevOuter, 0 , -hTop/2) $ Csg.scale (20,14, 8) $ Csg.unitCube
+
+rotateDialIntoPlace :: Csg.BspTree -> Csg.BspTree
+rotateDialIntoPlace = Csg.translate (0.5*dTop, 0, 0) . Csg.rotate (0, 1, 0) panAngle . Csg.translate (0.5*dPan, 0, rBevOuter)
+
+dialPositive = let r = rDial+ dialClearance + wallT in 
+                rotateDialIntoPlace $ Csg.scale (r, r, wallT + dialInset) $  Csg.translate (0, 0, -0.5) $ Csg.unitCylinder 16
+
+dialNegative = let r = rDial+ dialClearance in 
+                rotateDialIntoPlace $ ((Csg.scale (r, r, 2* dialInset) $ Csg.unitCylinder 32) `Csg.union` (Csg.scale (shaftR, shaftR, 40) $ Csg.unitCylinder 12))
+
+
+
+holePositions = [ (x,y,0) | x <- [10, dTotal -10 - dTop/2], y <- ( * (wPan + rBevInner-3)) <$> [-0.5, 0.5]]
+
+baseAttachment = let rHole = 3/2
+                     rOuter = 5 
+                     baseHeight = 5
+                     cyl = Csg.scale (rOuter, rOuter, (hTop-baseHeight)*2) $ Csg.unitCylinder 16
+                     col = cyl `Csg.subtract` (Csg.scale (rHole, rHole, 1000) $ Csg.unitCylinder 8)
+                  in Csg.unionConcat $ (`Csg.translate`  col) <$> holePositions
+
+object :: Csg.BspTree
+object = ((basicShape rBevOuter `Csg.subtract` (Csg.translate (0, 0, -eps) $ (basicShape rBevInner `Csg.subtract` baseAttachment))) `Csg.union` dialPositive) `Csg.subtract` ( ledHoles `Csg.union` usbHole `Csg.union` dialNegative)
+
+
+
+base = let h = 2 
+           rScrew = 3/2
+           screw = Csg.scale (rScrew, rScrew, 1000) $ Csg.unitCylinder 8
+        in (Csg.translate ((dTotal-dTop)/2, 0, -hTop -20) $ (Csg.unionConcat ([
+         Csg.scale (dTotal,wPan + rBevInner *2, h) $ Csg.unitCube,
+         Csg.scale (dTotal+rBevInner *2,wPan, h) $ Csg.unitCube
+       ] <>
+         [Csg.translate (dTotal *i, wPan*j, 0) $ Csg.scale (rBevInner, rBevInner, h) $ cylinder | i <- [-0.5, 0.5], j <- [-0.5, 0.5]]
+        ))) `Csg.subtract` (Csg.unionConcat $ (`Csg.translate` screw) <$> holePositions)
 
 path = "rotary-encoder.stl"
+pathBase = "rotary-encoder-base.stl"
+
 
 main :: IO ()
-main = T.writeFile path $ Csg.STL.toSTL object
+main = do 
+  --T.writeFile path $ Csg.STL.toSTL object
+  T.writeFile pathBase $ Csg.STL.toSTL base
